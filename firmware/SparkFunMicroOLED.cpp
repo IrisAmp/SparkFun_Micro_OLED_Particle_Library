10092cc1ea949bdb15f4d1bc7ea5e5e8c4de1adf
IrisAmp <brandon.d.yue@gmail.com>
2015-12-24 02:08:47 -0700

    Changed the name of the swap macro so that it doesn't clash with std

--- firmware/SparkFunMicroOLED.cpp
+++ firmware/SparkFunMicroOLED.cpp
@@ -407,13 +407,13 @@ void MicroOLED::line(uint8_t x0, uint8_t y0, uint8_t x1, uint8_t y1) {
 void MicroOLED::line(uint8_t x0, uint8_t y0, uint8_t x1, uint8_t y1, uint8_t color, uint8_t mode) {
 	uint8_t steep = abs(y1 - y0) > abs(x1 - x0);
 	if (steep) {
-		swap(x0, y0);
-		swap(x1, y1);
+		sparkFun_swap(x0, y0);
+		sparkFun_swap(x1, y1);
 	}
 
 	if (x0 > x1) {
-		swap(x0, x1);
-		swap(y0, y1);
+		sparkFun_swap(x0, x1);
+		sparkFun_swap(y0, y1);
 	}
 
 	uint8_t dx, dy;
